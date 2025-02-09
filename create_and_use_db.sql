543b3d9b3347ed919396561d2df8895b2f5a2812
edelson018 <enriquezedelson52@gmail.com>
2025-02-10 01:09:19 +0800

    Update create_and_use_db.sql

--- create_and_use_db.sql
+++ create_and_use_db.sql
@@ -1,7 +1,6 @@
--- Step 1: Create a database called UniversityDB
+-- Create a database called UniversityDB
 CREATE DATABASE `UniversityDB`;
--- Step 2: Use the newly created database
-USE `UniversityDB`;
--- Verification (Optional): 
+-- Use the newly created database
+USE `UniversityDB`; 
 -- You can add a simple query to verify the database is selected.
 SELECT DATABASE(); -- This will show the currently selected database.
